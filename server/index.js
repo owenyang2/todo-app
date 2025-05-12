33fb584513023b4a99fc1c5f772f895c8ad20d45
Owen <82738322+owenyang2@users.noreply.github.com>
2025-05-11 22:43:56 -0400

    Setup PostgreSQL database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import pool from './db.js';
 
 const app = express();
 
 // Middleware
 app.use(cors()); // allows all origins by default
-app.use(express.json()); // parse incoming json req
+app.use(express.json()); // parse incoming json req.body
+
+// Routes
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 app.listen(5000, () => {
     console.log("Server stared on port 5000");
